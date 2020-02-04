5f97ff03c8f58f65f512d79b47448afc61911c90
francielenf <franciielenf@gmail.com>
2020-02-04 13:36:00 -0300

    Atualizando projeto

--- src/test/java/Suporte/Generator.java
+++ src/test/java/Suporte/Generator.java
@@ -4,6 +4,7 @@ import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 
 public class Generator {
+
     public static String dataHoraParaArquivo(){
         Timestamp ts = new Timestamp(System.currentTimeMillis());
         return new SimpleDateFormat("yyyyMMddhhmmss").format(ts);
