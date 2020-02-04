8496d4432f6e6fb6e4723acf43d38af3c20d68a0
Franciele Nascente de Fagundes <franciele.fagundes@gx2.local>
2020-02-04 09:14:13 -0300

    Page objects

---
+++ src/test/java/Suporte/Web.java
@@ -0,0 +1,20 @@
+package Suporte;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class Web {
+    public static WebDriver createChrome() {
+
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\franciele.fagundes\\Documents\\Driver\\chromedriver.exe");
+        WebDriver navegador = new ChromeDriver();
+        navegador.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        //Navegador para página do Taskit
+        navegador.get("http://www.juliodelima.com.br/taskit");
+        return navegador;
+    }
+}
---
+++ src/test/java/pages/LoginFormPage.java
@@ -0,0 +1,10 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class LoginFormPage {
+    private WebDriver navegador;
+
+    //public LoginFormPage(WebDriver navegador){
+       // this.navegador=navegador;
+}
